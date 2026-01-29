ee3969480c531c132db1ae1f5c62b905f00c2daa
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-29 18:22:49 +0700

    Delete DAta

--- AlgorithmPlan/Controllers/WeatherForecastController.cs
+++ AlgorithmPlan/Controllers/WeatherForecastController.cs
@@ -6,12 +6,7 @@ namespace AlgorithmPlan.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
+        
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
